8db6de36cb29929cd21749f06edc403f543a80ba
Swarup Thamke <118194258+swarupt29@users.noreply.github.com>
2024-05-27 18:32:44 +0530

    Update CategoryServiceImpl.java

--- src/main/java/com/nimap/service/CategoryServiceImpl.java
+++ src/main/java/com/nimap/service/CategoryServiceImpl.java
@@ -3,42 +3,50 @@ package com.nimap.service;
 import com.nimap.model.Category;
 import com.nimap.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-	@Autowired
-	private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-	@Override
-	public List<Category> getAllCategories() {
-		return categoryRepository.findAll();
-	}
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 
-	@Override
-	public Category getCategoryById(Long id) {
-		return categoryRepository.findById(id).orElse(null);
-	}
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
 
-	@Override
-	public Category createCategory(Category category) {
-		return categoryRepository.save(category);
-	}
+    @Override
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 
-	@Override
-	public Category updateCategory(Long id, Category category) {
-		if (categoryRepository.existsById(id)) {
-			category.setId(id);
-			return categoryRepository.save(category);
-		} else {
-			return null;
-		}
-	}
+    @Override
+    public Category updateCategory(Long id, Category category) {
+        if (categoryRepository.existsById(id)) {
+            category.setId(id);
+            return categoryRepository.save(category);
+        } else {
+            return null;
+        }
+    }
 
-	@Override
-	public void deleteCategory(Long id) {
-		categoryRepository.deleteById(id);
-	}
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Category> getAllCategories(Pageable pageable) {
+        return categoryRepository.findAll(pageable);
+    }
 }
