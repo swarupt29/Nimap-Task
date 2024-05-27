e5292a69248fabad627b3925a5ff4a33fce71e02
Swarup Thamke <118194258+swarupt29@users.noreply.github.com>
2024-05-27 18:33:13 +0530

    Update CategoryService.java

--- src/main/java/com/nimap/service/CategoryService.java
+++ src/main/java/com/nimap/service/CategoryService.java
@@ -1,18 +1,21 @@
-
 package com.nimap.service;
 
 import com.nimap.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CategoryService {
-	List<Category> getAllCategories();
+    List<Category> getAllCategories();
 
-	Category getCategoryById(Long id);
+    Category getCategoryById(Long id);
 
-	Category createCategory(Category category);
+    Category createCategory(Category category);
 
-	Category updateCategory(Long id, Category category);
+    Category updateCategory(Long id, Category category);
 
-	void deleteCategory(Long id);
+    void deleteCategory(Long id);
+
+    Page<Category> getAllCategories(Pageable pageable);
 }
